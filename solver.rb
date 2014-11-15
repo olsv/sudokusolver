0df44ab5b065a38cb9e6a479c0c637945cc89b4b
Oleg Sviridovitch <oleg.sviridovitch@gmail.com>
2014-11-15 22:15:17 +0300

    first solver

---
+++ solver.rb
@@ -0,0 +1,106 @@
+#!/usr/bin/env ruby
+require 'pry'
+require 'pry-nav'
+
+class Cell
+  attr_accessor :value, :group, :row, :col, :possible
+
+  def initialize(value, row = nil, col=nil, group=nil)
+    @value = value
+    @possible = []
+    @row = row
+    @col = col
+    @group = group
+  end
+end
+
+RANGE = (1..9).to_a
+
+class Row < Array
+
+  def <<(value)
+    raise StandardError, 'only Cell allowed as value' unless value.is_a? Cell
+    super value
+  end
+
+  def []=(*attrs)
+    raise StandardError, 'only Cell allowed as value' unless attrs.last.is_a? Cell
+    super *attrs
+  end
+
+  def values
+    self.map(&:value)
+  end
+
+  def unsolved_cells
+    self.reject{|cell| cell.value > 0 }
+  end
+
+  def solved?
+    @solved ||= possible.empty?
+  end
+
+  def allocated
+    values.select{|r| r > 0 }
+  end
+
+  def possible
+    (RANGE - allocated).sort
+  end
+
+end
+
+class Col < Row; end
+class Group < Row; end
+
+class Field
+  attr_reader :rows, :cols, :groups
+  ROWS = 9
+
+  def initialize(source)
+    @rows = []
+    @cols = []
+    @groups = []
+    source =  File.open(source)
+    (0..ROWS - 1).each do |i|
+      line = source.readline
+      @rows[i] ||= Row.new
+      (0..ROWS - 1).each do |j|
+        @cols[j] ||= Col.new
+        gid = "#{i/3}#{j/3}".to_i(3)
+        @groups[gid] ||= Group.new
+        cell = Cell.new(line[j].to_i, @rows[i], @cols[j], @groups[gid])
+        @rows[i] << cell
+        @cols[j] << cell
+        @groups[gid] << cell
+      end
+    end
+  end
+
+  def solve
+    groups.reject{|r| r.solved? }.each do |group|
+      group.unsolved_cells.each do |cell|
+        possible = RANGE - group.values - cell.row.values - cell.col.values
+        possible.count > 1 ? cell.possible = possible : cell.value = possible.first
+      end
+      group.solved?
+    end
+    solve if groups.reject{|r| r.solved? }.any?
+  end
+
+  def valid?
+    self.rows.map(&:values).all?{|r| r.uniq.sort.eql? RANGE } &&
+    self.cols.map(&:values).all?{|r| r.uniq.sort.eql? RANGE } &&
+    self.groups.map(&:values).all?{|r| r.uniq.sort.eql? RANGE }
+  end
+end
+
+t = Time.now
+f = Field.new('source.txt')
+f.solve
+File.open('res.txt','w'){|file| file.write f.rows.map(&:values).map(&:join).join("\n")}
+p f.rows.map(&:values).map(&:join).join("\n")
+p Time.now - t
+
+
+
